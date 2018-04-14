b4453821f3601a3462c3ddb0832565d9df192f38
rupalnahar <rnahar@andrew.cmu.edu>
2018-04-14 18:28:14 -0400

    staticssss

--- youtube.py
+++ youtube.py
@@ -24,6 +24,7 @@ def videos_list_by_id(client, **kwargs):
   response = client.videos().list(
     **kwargs
   ).execute()
+  return response
   #return print_response(response)
 
 def search_list_by_keyword(client, **kwargs):
@@ -51,18 +52,31 @@ client = get_authenticated_service()
 #f = open("surfind_videos.txt","w+")
 
 numVids = 0
-while(numVids < 10):
+search_videos = []
+while(numVids < 3):
   randId = getRandomId()
   result = search_list_by_keyword(client,
       		part='snippet',
       		maxResults=50,
       		q=randId,
-      		type='')
+      		type='',
+          relevanceLanguage = 'in')
+
   for vid in result['items']:
     if 'videoId' in vid['id'].keys():
       if (vid['id']['videoId'][:videoIdLen]) == randId:
-        print(vid)
+        search_videos.append(vid['id']['videoId'])
+        print(vid['id']['videoId'])
         numVids += 1
+  video_ids = ','.join(search_videos)
+# print("BLAH")
+# print(search_videos)
+# print(video_ids)
+video_response = videos_list_by_id(client,
+  id=video_ids,
+  part='snippet, statistics')
+
+print(video_response)
 		
 	#print(type(result))
 	#f.write(json.dumps(result))
